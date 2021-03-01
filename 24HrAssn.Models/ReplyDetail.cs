700cdbe589301d5fcf561b723a4cbea8f1e1fab6
Ashley Lawrence <ashleylawrence836@mail.com>
2021-03-01 18:39:34 -0500

    model classes

---
+++ 24HrAssn.Models/ReplyDetail.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HrAssn.Models
+{
+    public class ReplyDetail
+    {
+        public int NoteId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        [Display(Name = "Reply By")]
+        public Guid Author { get; set; }
+
+        [Display(Name = "Reply")]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        [Display(Name = "Edited")]
+        public DateTimeOffset? EditUtc { get; set; }
+    }
+}
