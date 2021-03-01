fe068b47841cb1c3ccf16412a936fc178810070a
Ashley Lawrence <ashleylawrence836@mail.com>
2021-03-01 16:03:20 -0500

    initial

---
+++ 24HrAssn.Data/Comment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HrAssn.Data
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Text { get; set; }
+        [Required]
+        public Guid Author { get; set; }
+        // Virtual list of replies
+        // foreign key to post via Id w/ virtual post
+
+    }
+}
---
+++ 24HrAssn.Data/Post.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HrAssn.Data
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public Guid Author { get; set; }
+        public string Text { get; set; }
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset EditUtc { get; set; }
+
+        // Virtual list of comments
+
+    }
+}
