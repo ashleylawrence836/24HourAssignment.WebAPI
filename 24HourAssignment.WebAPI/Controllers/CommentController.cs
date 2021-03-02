b010d3e6760bd047b4d6fc45bd138d912a501fcb
Daniel <dordan0401@gmail.com>
2021-03-02 08:32:19 -0500

    Trying to fix git

---
+++ 24HourAssignment.WebAPI/Controllers/CommentController.cs
@@ -0,0 +1,14 @@
+﻿using _24HourAssignment.WebAPI.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace _24HourAssignment.WebAPI.Controllers
+{
+    public class CommentController : ApiController
+    {
+    }
+}
